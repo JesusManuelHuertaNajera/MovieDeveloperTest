5c520687af035468e5451b42e3708526b6bff4ec
Jesus Manuel Huerta Najera <61481286+JesusManuelHuertaNajera@users.noreply.github.com>
2025-10-15 09:04:32 -0600

    fix bug

--- mymovies/lib/content/config/api/config.dart
+++ mymovies/lib/content/config/api/config.dart
@@ -1,5 +1,5 @@
 class ApiValues {
   static String baseUrl = 'https://api.themoviedb.org';
   static String baseUrlImage = 'https://image.tmdb.org';
-  static String apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjZiNzk2MDUxMzUyN2Y5ZWFhNzFjNTZlYTNmMWYwYiIsIm5iZiI6MTc2MDUwMzUwNy43Nywic3ViIjoiNjhlZjI2ZDM3ZWZhYzU5NWM3ZjBhMDEwIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.5PtQSWy_i9j2tKSfJYOSuOlbKnDwN8rMqMIPSZuDjvk';
+  static String apiKey = '';
 }
